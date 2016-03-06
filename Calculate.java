e05e3964fd0243fe15047c5a5023fcda07e9a17e
Stanislav Derkach <Stanislav Derkach>
2016-03-06 04:08:56 +0200

    New Calculator

--- Calculate.java
+++ Calculate.java
@@ -1,11 +1,62 @@
+
+
+import java.util.Scanner;
+
+
 public class Calculate {
-public static void main (String[] args){
-System.out.println("Calculate...");
-int first = Integer.valueOf(args[0]);
-int second = Integer.valueOf(args[1]);
-int summ = first + second;
-int multiplication = first * second;
-System.out.println("The summ is " + summ);
-System.out.println("The multiplication is " + multiplication);
+    private int result = 0;
+    private int num = 0;
+    String action = "no";
+
+
+    Scanner scanner = new Scanner(System.in);
+
+    public Calculate() {
+    }
+
+    public void addresult(int... result) {
+
+
+        do {
+            System.out.println("Select an Action:\n1-Summ.\n2-Subtraction.\n3-Multiplication.\n4-Division.");
+            num = Integer.parseInt(scanner.next());
+            switch (num) {
+                case 1:
+                    for (int sum : result)
+                        this.result += sum;
+                    break;
+                case 2:
+                    for (int sum : result)
+                        this.result -= sum;
+                    break;
+                case 3:
+                    this.result = 1;
+                    for (int sum : result)
+                        this.result *= sum;
+                    break;
+                case 4:
+                    this.result = 1;
+                    for (int sum : result)
+                        this.result /= sum;
+                    break;
+            }
+            getResult();
+            resetResult();
+            System.out.println("yes-repeat, no-end");
+            action = scanner.next();
+        }
+        while (!action.equals("no"));
+    }
+
+    public void getResult() {
+        System.out.println(result);
+    }
+
+    public void resetResult() {
+        result = 0;
+        num = 0;
+    }
 }
-}
+
+
+
---
+++ Calculator.java
@@ -0,0 +1,9 @@
+
+
+public class Calculator {
+    public static void main(String[] args) {
+        Calculate calculate = new Calculate();
+        calculate.addresult(30, 20, 5);
+
+    }
+}

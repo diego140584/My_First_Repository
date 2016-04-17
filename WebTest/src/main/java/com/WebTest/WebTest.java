598b2ab85b8b5c6f238709541852d1bca97c2066
stas140584 <becks.ua@gmail.com>
2016-04-17 21:54:16 +0300

    git commit -m "First Servlet"

---
+++ WebTest/src/main/java/com/WebTest/WebTest.java
@@ -0,0 +1,20 @@
+package com.WebTest;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Stas on 17.04.2016.
+ */
+public class WebTest extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+servletResponse.setContentType("text/html");
+        PrintWriter writer = servletResponse.getWriter();
+        writer.println("This is my first Servlet");
+    }
+}
